f0ad43c45ecc628c9d5ee2637736bea61bd24aff
hhs-rm51 <diegoangel00@live.com>
2017-01-30 09:23:04 -0800

    added a new line

--- app/src/main/java/com/example/hhs_rm51/gitts/MainActivity.java
+++ app/src/main/java/com/example/hhs_rm51/gitts/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //ethan
+    //whats up
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
