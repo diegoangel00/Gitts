8927dd1db5710f81ab0ec61aa4c25c01dd64b1a6
hhs-rm51 <hhs-rm51@husd.com>
2017-01-30 09:17:54 -0800

    Initial commit

---
+++ app/src/main/java/com/example/hhs_rm51/gitts/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hhs_rm51.gitts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //ethan
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
